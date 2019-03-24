5a97da48c9d933b1fb34c50ac392e1ec901c1e69
Raíssa Faccioli <raissafaccioli@outlook.com>
2019-03-24 18:19:22 -0300

    add mock pictures for ngx-gallery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ProductSpecificationComponent } from './product-specification/product-specification.component';
+import { NgxGalleryModule } from 'ngx-gallery';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { ProductSpecificationComponent } from './product-specification/product-s
     ProductSpecificationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxGalleryModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/product-specification/product-specification.component.ts
+++ src/app/product-specification/product-specification.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from './product.interface';
+import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gallery';
 
 @Component({
   selector: 'app-product-specification',
@@ -7,6 +8,9 @@ import { Product } from './product.interface';
   styleUrls: ['./product-specification.component.scss']
 })
 export class ProductSpecificationComponent implements OnInit {
+
+  galleryOptions: NgxGalleryOptions[];
+  galleryImages: NgxGalleryImage[];
   
   products: Product = {
     _id: '1', 
@@ -16,10 +20,50 @@ export class ProductSpecificationComponent implements OnInit {
     energeticValue: 12, 
     protein: 191
   };
-
   constructor() { }
 
   ngOnInit() {
-  }
+    this.galleryOptions = [
+      {
+        width: '600px',
+        height: '400px',
+        thumbnailsColumns: 4,
+        imageAnimation: NgxGalleryAnimation.Slide
+      },
+      // max-width 800
+      {
+        breakpoint: 800,
+        width: '100%',
+        height: '600px',
+        imagePercent: 80,
+        thumbnailsPercent: 20,
+        thumbnailsMargin: 20,
+        thumbnailMargin: 20
+      },
+      // max-width 400
+      {
+        breakpoint: 400,
+        preview: false
+      }
+    ];
 
+    this.galleryImages = [
+        {
+            small: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTm3CuNzOrmWhos3MXI0v_KkyO_xrZ5Y8hFxSYGj_6OtUygZUUX',
+            medium: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTm3CuNzOrmWhos3MXI0v_KkyO_xrZ5Y8hFxSYGj_6OtUygZUUX',
+            big: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTm3CuNzOrmWhos3MXI0v_KkyO_xrZ5Y8hFxSYGj_6OtUygZUUX'
+        },
+        {
+            small: 'https://www.w3schools.com/w3css/img_lights.jpg',
+            medium: 'https://www.w3schools.com/w3css/img_lights.jpg',
+            big: 'https://www.w3schools.com/w3css/img_lights.jpg'
+        },
+        {
+            small: 'https://upload.wikimedia.org/wikipedia/commons/3/39/Sunflower_Helianthus_1_edited.png',
+            medium: 'https://upload.wikimedia.org/wikipedia/commons/3/39/Sunflower_Helianthus_1_edited.png',
+            big: 'https://upload.wikimedia.org/wikipedia/commons/3/39/Sunflower_Helianthus_1_edited.png'
+        }
+    ];
+  }
 }
+
